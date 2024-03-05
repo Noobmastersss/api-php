fa544f90f8cb9f0dba2066a2e23434c9084692fe
Noobmastersss <161553492+Noobmastersss@users.noreply.github.com>
2024-03-05 08:34:59 -0800

    Update api.php

--- api.php
+++ api.php
@@ -30,6 +30,50 @@ $internet = [
      "velocidad de coneccion" => "Gbps",
      "acceso inalambrico" => "WI-FI,Bluetooth",
      "protocolosde comunicacion" => "proyector DELL",
+    <?php
+// Agregamos los objetos
+$proyector = [
+    "nombre" => "proyector DELL",
+    "resolucion" => "alta definicion full hd, 4k",
+    "brillo" => 200,
+    "tecnologia de proyector" => "LCD",
+    "ratio de aspecto" => 18,9,
+    "pantalla proyectada" => 120,
+    "conectividad " => "USB,HDMI,VGA",
+    "COMPATIVILIDAD 3D" => 100.00,
+    "zoom optico" => 10.00,
+    "correccion keystone" => "ajuste de imagen",
+    "duracion de lampara " => 200 hrs,
+    "nivel de ruido" => 2,
+    "funciones de red" =>  "transmision de datos",
+    "peso" => 2.250 kg,
+    "tamaño" => 20 cm4,
+    "funciones de pantalla dividida" => "multi funciones",
+    "funciones de seguridad" => "bloqueo de contraseña",
+    "garantia" => "un año despues de la compra",
+    "portatividad " => "practico de llevar",
+    "montaje" => "facil de montar en diferentes cituasiones",
+    // Puedes agregar más atributos aquí
+    ];
+
+$internet = [
+     "nombre" => "izzi",
+     "conectividad" => "largo alcanse",
+     "velocidad de coneccion" => "Gbps",
+     "acceso inalambrico" => "WI-FI,Bluetooth",
+     "protocolosde comunicacion" => "proyector DELL",
+    
+    ];
+
+    // Creamos una lista de objetos en un arreglo
+$tecnologia = [
+    $proyector,
+    $laptop,
+    $telefono,
+];
+
+// Creamos un arreglo de objetos relacionados por nombre
+$tecnologia_productos = [];
     ];
 
     // Creamos una lista de objetos en un arreglo
